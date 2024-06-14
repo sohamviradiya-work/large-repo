97985f32943b4b327c9164367511cefed0dc232f
sohamviradiya.work@gmail.com <sohamviradiya.work@gmail.com>
2024-06-14 14:24:02 +0530

    added sub project 71

---
+++ project71/src/test/java/com/module4/TestClass3.java
@@ -0,0 +1,57 @@
+package com.module4;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestClass3 {
+
+    @Test
+    void testMethod0() {
+        assertEquals(0, 0);
+    }
+
+    @Test
+    void testMethod1() {
+        assertEquals(7, 7);
+    }
+
+    @Test
+    void testMethod2() {
+        assertEquals(9, 9);
+    }
+
+    @Test
+    void testMethod3() {
+        assertEquals(5, 5);
+    }
+
+    @Test
+    void testMethod4() {
+        assertEquals(5, 5);
+    }
+
+    @Test
+    void testMethod5() {
+        assertEquals(4, 4);
+    }
+
+    @Test
+    void testMethod6() {
+        assertEquals(6, 6);
+    }
+
+    @Test
+    void testMethod7() {
+        assertEquals(6, 6);
+    }
+
+    @Test
+    void testMethod8() {
+        assertEquals(4, 4);
+    }
+
+    @Test
+    void testMethod9() {
+        assertEquals(3, 3);
+    }
+}
